04f19fcd21deb733c61e367a485c3c7ddeb7229f
sina_yzsn <sina.yzsn@gmail.com>
2023-06-15 19:45:56 +0330

    Initilazed the beta stage of music sorter
    The main thing that this beta version would do will be described in the
    Read me file but for this commit it will also be some elaboration.
    
    This beta version has one main function. The main requirement of this
    bot is a super group that has the genres that you'll need as a topic.
    for example:
        - I have created a super group with the name of playlist.
        - I have created the genres as a tpoic:
            - Rock
            - Metal
            - Pop
            - Rap
            ....
        - I have found the Tpoic ID via the telegram API or the link
          generated by the link available by copying the link of the
          message. (Read more on the README).
        - After that I would add the topic id to the code.
        - Now just send some music to the bot and caategorize them.

--- main.py
+++ main.py
@@ -1,64 +1,158 @@
-import telebot
-import re
-from dotenv import load_dotenv
-import os
+from telegram import Update, ReplyKeyboardMarkup, ReplyKeyboardRemove
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    MessageHandler,
+    filters,
+    ConversationHandler,
+    ContextTypes
+)
+import logging
+import environments as KEY
+from typing import Final
 
-load_dotenv()
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-GROUP_CHAT_ID = os.getenv("GROUP_CHAT_ID")
-bot = telebot.TeleBot(f"{BOT_TOKEN}")
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO, filename='bot.log'
+)
+logger = logging.getLogger(__name__)
 
-# Define the group chat ID
-group_chat_id = f'{GROUP_CHAT_ID}'
-# Define a dictionary of hashtags and their corresponding genres
-genres = {'#pop': 'Pop',
-          '#rock': 'Rock',
-          '#rap': 'Rap',
-          '#country': 'Country'}
-
-
-# Define a function to extract the hashtags from a message
-def extract_hashtags(message):
-    hashtags = re.findall(r'\#\w+', message.text)
-    print("extract_hashtags works")
-    return hashtags
+TOKEN: Final = KEY.BOT_TOKEN
+BOT_USERNAME: Final = KEY.BOT_ID
+GROUP: Final = KEY.GROUP_CHAT_id
+genre = [["POP", "ROCK", "RAP", "METAL", "COUNTRY", "ALT_METAL"]]
+topics = {
+    "ROCK": 38,
+    "METAL": 47,
+    "POP": 52,
+    "RAP": 54,
+    "COUNTRY": 56,
+    "ALT_METAL": 58
 
 
-# Define a handler for messages in the "sorting" group
-@bot.message_handler(func=lambda message: message.chat.type == 'supergroup' and message.chat.title == 'sorting')
-def handle_sorting_message(message):
-    # Extract hashtags and song name from the message
-    hashtags = extract_hashtags(message)
-    song_name = message.text
-    print("extract_hashtags works")
 
-    # If the message contains a song name, use it to determine the genre
-    if song_name:
-        for genre, keywords in genres.items():
-            if any(keyword in song_name.lower() for keyword in keywords.split()):
-                hashtags.append(genre)
-                print("extract_hashtags works [if song name]")
-    # If there are no hashtags or genres, ignore the message
-    if not hashtags:
-        print("extract_hashtags works [if not in hashtag]")
-        return
+}
+GENRE, CATEGORIZE_SONG = range(2)
 
-    # Sort messages based on hashtags and genres
-    sorted_messages = {}
-    for hashtag in hashtags:
-        if hashtag in genres:
-            topic = genres[hashtag]
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Starts the conversation and asks the user to send a AUDIO file."""
+    message = update.message
+    await update.message.reply_text(
+        "Hi! My name is Music Sorter Bot. I will hold a conversation with you. "
+        "Send /cancel to stop talking to me.\n\n"
+        "Send me a song and the genre so I could categorize it."
+    )
+    return GENRE
+
+
+async def genre_selection(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """
+    This function would receive an audio file and would pass it to the next function (categorize_song) here is how it
+    works:
+    1. first it would receive a message and checks if it is a valid audio file.
+    2. if it is a valid audio file then it would save the message id and reply to it with options that are available
+        in the genre variable.
+    3. The user would choose between the available options and the next function would be called.
+    """
+    message = update.message
+
+    if message.audio is not None:
+        # Save the audio message ID
+        context.user_data["audio_message_id"] = message.message_id
+
+        await message.reply_text(
+            "Please choose a genre:",
+            reply_markup=ReplyKeyboardMarkup(
+                genre, one_time_keyboard=True, input_field_placeholder="Please choose"
+            )
+        )
+        return CATEGORIZE_SONG
+    else:
+        await message.reply_text("This bot only accepts audio files")
+        return GENRE
+
+
+async def categorize_song(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """
+    This function would be called after the user have chosen a genre.
+        - First it would check if the chosen genre is available in the topic dict.
+        - it would get the audio message id from the previous function.
+        - checks if the id is not NONE.
+        - Prints (Song categorized successfully!)
+    if the message id is NONE it prints (No audio file found.)
+    and if none
+    """
+    chosen_genre = update.message.text
+    user_id = update.effective_user.id
+
+    if chosen_genre in topics:
+        destination_thread_id = topics[chosen_genre]
+
+        # Retrieve the audio message ID from user_data
+        audio_message_id = context.user_data.get("audio_message_id")
+
+        if audio_message_id:
+            await context.bot.forward_message(
+                chat_id=GROUP,
+                from_chat_id=user_id,
+                message_id=audio_message_id,
+                message_thread_id=destination_thread_id
+            )
+            await update.message.reply_text("Song categorized successfully!")
         else:
-            topic = hashtag
-        if topic in sorted_messages:
-            sorted_messages[topic].append(message.text)
-        else:
-            sorted_messages[topic] = [message.text]
+            await update.message.reply_text("No audio file found.")
+    else:
+        await update.message.reply_text("Invalid genre.")
 
-    # Send the sorted messages to the user who requested the songs
-    for topic, messages in sorted_messages.items():
-        message_text = f'#{topic}\n\n' + '\n\n'.join(messages)
-        bot.send_message(message.chat.id, message_text)
-    
-# Start the bot
-bot.polling()
+    return GENRE
+
+
+async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Cancels and ends the conversation."""
+    user = update.message.from_user
+    logger.info("User %s canceled the conversation.", user.first_name)
+    await update.message.reply_text(
+        "Bye! I hope we can talk again some day.", reply_markup=ReplyKeyboardRemove()
+    )
+
+    return ConversationHandler.END
+
+
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    help_msg = """
+    This is a help command output that provides information on how to use the bot.
+
+    Available commands:
+    - /start: Start the conversation with the bot.
+    - /help: Get help and information about the bot.
+    - /cancel: Cancel the conversation.
+
+    How to use:
+    - Send a song and choose a genre to categorize it.
+
+    """
+    await update.message.reply_text(help_msg)
+
+
+def main() -> None:
+    app = Application.builder().token(TOKEN).build()
+
+    conversation_handler = ConversationHandler(
+        entry_points=[CommandHandler("start", start)],
+        states={
+            GENRE: [MessageHandler(filters.AUDIO, genre_selection)],
+            CATEGORIZE_SONG: [MessageHandler(filters.TEXT, categorize_song)],
+        },
+        fallbacks=[CommandHandler("cancel", cancel)],
+    )
+    app.add_handler(conversation_handler)
+
+    app.add_handler(CommandHandler("help", help_command))
+    # app.run()
+    app.run_polling()
+
+
+if __name__ == '__main__':
+    print('Polling...')
+    # Run the bot
+    main()
