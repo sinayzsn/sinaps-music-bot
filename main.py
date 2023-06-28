6b27d6a0dd552284bb53b7a74766f9b6b18f249b
sina_yzsn <sina.yzsn@gmail.com>
2023-06-28 20:52:40 +0330

    TestingMultipleMessageHandling
    Te testing is for the storing of the message ID's of the songs that are being sent to the bot. The bot should be able to save all the ID's and then forward them all to the related topic.
    
    The line that is going to be changed is:
    
    65-67
    # context.user_data.setdefault("audio_message_ids", []).append(message.message_id)
    for ids in message.message_ids:
        context.user_data.setdefault("audio_message_ids", []).append(ids)

--- main.py
+++ main.py
@@ -63,7 +63,8 @@ async def genre_selection(update: Update, context: ContextTypes.DEFAULT_TYPE) ->
         # In this method unlike the above method it can store multiple messages id's. That would be used
         # later to forward them.
         # context.user_data.setdefault("audio_message_ids", []).append(message.message_id)
-        context.user_data["audio_message_ids"].append(message.message_id)
+        for ids in message.message_ids:
+            context.user_data.setdefault("audio_message_ids", []).append(ids)
         logger.info("Getting the message Id's")
         logger.info(context.user_data.get("audio_message_ids"))
         await message.reply_text(
